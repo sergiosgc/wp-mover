7fd0d80d658fb9ccd095cc1737cb8f97406a6250
Sérgio Carvalho <sergio.carvalho@sooma.com>
2023-05-25 14:38:26 +0100

    Automatically create category map for all taxonomies; Set all taxonomies on sync_posts; Map featured image using media map on sync_posts; Set all custom fields; sync translations along with posts;

--- snippets.right/enable_rest_toolset.php
+++
@@ -1,1 +0,0 @@
-add_filter( 'toolset_rest_run_exposure_filters', '__return_true' );
--- snippets.right/show_in_rest.php
+++
@@ -1,4 +0,0 @@
-add_filter('register_post_type_args', function($args, $post_type) {
-	$args['show_in_rest'] = true;
-	return $args;
-}, 10, 2);

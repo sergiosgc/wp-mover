7ed27033aa1741641c6b309addb4f173bb4264ef
Sérgio Carvalho <sergio.carvalho@sooma.com>
2023-05-26 12:18:39 +0100

    Expand tabs in code

--- snippets.left/wp_mover.php
+++ snippets.left/wp_mover.php
@@ -1,47 +1,47 @@
 add_action( 'rest_api_init', function () {
-	foreach (array_keys(get_taxonomies()) as $taxonomy) register_rest_field( $taxonomy, 'translations', [
-		'get_callback' => function( $term ) {
-			$trid = apply_filters( 'wpml_element_trid', false, $term['id'], 'tax_category');
-			if (!$trid) return [];
-			$translations = apply_filters( 'wpml_get_element_translations', false, $trid, 'tax_category');
-			if (!$translations) return [];
-			$result = [];
-			foreach($translations as $lang => $tr) if ($tr->element_id != $term['id']) $result[$lang] = (int) $tr->element_id;
-			return $result;
-		},
+    foreach (array_keys(get_taxonomies()) as $taxonomy) register_rest_field( $taxonomy, 'translations', [
+        'get_callback' => function( $term ) {
+            $trid = apply_filters( 'wpml_element_trid', false, $term['id'], 'tax_category');
+            if (!$trid) return [];
+            $translations = apply_filters( 'wpml_get_element_translations', false, $trid, 'tax_category');
+            if (!$translations) return [];
+            $result = [];
+            foreach($translations as $lang => $tr) if ($tr->element_id != $term['id']) $result[$lang] = (int) $tr->element_id;
+            return $result;
+        },
         'schema' => [
             'description' => __( 'Taxonomy Translations.' ),
             'type'        => 'array'
         ]
-	]);
-	
-	$post_types = array_merge(['post', 'page'], array_keys(get_post_types( 
-		[
-			'public'   => true,
-       		'_builtin' => false,
-			
-		]
-		, 'names', 'and' )));
-	foreach ($post_types as $post_type) register_rest_field( $post_type, 'translations', [
+    ]);
+    
+    $post_types = array_merge(['post', 'page'], array_keys(get_post_types( 
+        [
+            'public'   => true,
+               '_builtin' => false,
+            
+        ]
+        , 'names', 'and' )));
+    foreach ($post_types as $post_type) register_rest_field( $post_type, 'translations', [
         'get_callback' => function( $post ) {
-			$type = apply_filters( 'wpml_element_type', get_post_type( $post['id'] ) );
-			$translations = apply_filters( 
-				'wpml_get_element_translations', 
-				array(), 
-				apply_filters( 'wpml_element_trid', false, $post['id'], $type ), 
-				$type );
-			
-			$result = [];
-			foreach($translations as $lang => $tr) if ($tr->element_id != $post['id']) $result[$lang] = (int) $tr->element_id;
-			return $result;
-		},
+            $type = apply_filters( 'wpml_element_type', get_post_type( $post['id'] ) );
+            $translations = apply_filters( 
+                'wpml_get_element_translations', 
+                array(), 
+                apply_filters( 'wpml_element_trid', false, $post['id'], $type ), 
+                $type );
+            
+            $result = [];
+            foreach($translations as $lang => $tr) if ($tr->element_id != $post['id']) $result[$lang] = (int) $tr->element_id;
+            return $result;
+        },
         'schema' => [
             'description' => __( 'Post Translations.' ),
             'type'        => 'array'
         ]
-	] );
+    ] );
 } );
 add_filter('register_post_type_args', function($args, $post_type) {
-	$args['show_in_rest'] = true;
-	return $args;
+    $args['show_in_rest'] = true;
+    return $args;
 }, 10, 2);
--- snippets.right/wp_mover.php
+++ snippets.right/wp_mover.php
@@ -1,75 +1,75 @@
 add_filter( 'toolset_rest_run_exposure_filters', '__return_true' );
 add_action( 'rest_api_init', function () {
-	foreach (array_keys(get_taxonomies()) as $taxonomy) register_rest_field( $taxonomy, 'translations', [
-		'get_callback' => function( $term ) {
-			$trid = apply_filters( 'wpml_element_trid', false, $term['id'], 'tax_category');
-			if (!$trid) return [];
-			$translations = apply_filters( 'wpml_get_element_translations', false, $trid, 'tax_category');
-			if (!$translations) return [];
-			$result = [];
-			foreach($translations as $lang => $tr) if ($tr->element_id != $term['id']) $result[$lang] = (int) $tr->element_id;
-			return $result;
-		},
+    foreach (array_keys(get_taxonomies()) as $taxonomy) register_rest_field( $taxonomy, 'translations', [
+        'get_callback' => function( $term ) {
+            $trid = apply_filters( 'wpml_element_trid', false, $term['id'], 'tax_category');
+            if (!$trid) return [];
+            $translations = apply_filters( 'wpml_get_element_translations', false, $trid, 'tax_category');
+            if (!$translations) return [];
+            $result = [];
+            foreach($translations as $lang => $tr) if ($tr->element_id != $term['id']) $result[$lang] = (int) $tr->element_id;
+            return $result;
+        },
         'schema' => [
             'description' => __( 'Taxonomy Translations.' ),
             'type'        => 'array'
         ]
-	]);
-	$post_types = array_merge(['post', 'page'], array_keys(get_post_types( 
-		[
-			'public'   => true,
-       		'_builtin' => false,
-			
-		]
-		, 'names', 'and' )));
-	
+    ]);
+    $post_types = array_merge(['post', 'page'], array_keys(get_post_types( 
+        [
+            'public'   => true,
+            '_builtin' => false,
+            
+        ]
+        , 'names', 'and' )));
+    
     foreach ($post_types as $post_type) register_rest_field( $post_type, 'translations', [
         'get_callback' => function( $post ) {
-			$type = apply_filters( 'wpml_element_type', get_post_type( $post['id'] ) );
-			$translations = apply_filters( 
-				'wpml_get_element_translations', 
-				array(), 
-				apply_filters( 'wpml_element_trid', false, $post['id'], $type ), 
-				$type );
-			
-			$result = [];
-			foreach($translations as $lang => $tr) if ($tr->element_id != $post['id']) $result[$lang] = (int) $tr->element_id;
-			return $result;
-		},
+            $type = apply_filters( 'wpml_element_type', get_post_type( $post['id'] ) );
+            $translations = apply_filters( 
+                'wpml_get_element_translations', 
+                array(), 
+                apply_filters( 'wpml_element_trid', false, $post['id'], $type ), 
+                $type );
+            
+            $result = [];
+            foreach($translations as $lang => $tr) if ($tr->element_id != $post['id']) $result[$lang] = (int) $tr->element_id;
+            return $result;
+        },
         'schema' => [
             'description' => __( 'Post Translations.' ),
             'type'        => 'array'
         ]
-	] );
+    ] );
 } );
 add_action( 'init', function() {
-	if (!function_exists('toolset_get_fields_for_element')) return;
-	$after_insert = function($post, $request) {
-		$toolset_fields = toolset_get_fields_for_element($post->ID, \OTGS\Toolset\Common\PublicAPI\ElementDomain\POSTS);
-		
-		if (isset($request['toolset-meta'])) foreach($request['toolset-meta'] as $field_group => $fields) foreach($fields as $field_name => $field_value) if ($field_value['raw'] && array_key_exists($field_name, $toolset_fields)) {
-			$value = $field_value['raw'];
-			update_post_meta($post->ID, "wpcf-{$field_name}", $value);
-		}
+    if (!function_exists('toolset_get_fields_for_element')) return;
+    $after_insert = function($post, $request) {
+        $toolset_fields = toolset_get_fields_for_element($post->ID, \OTGS\Toolset\Common\PublicAPI\ElementDomain\POSTS);
+        
+        if (isset($request['toolset-meta'])) foreach($request['toolset-meta'] as $field_group => $fields) foreach($fields as $field_name => $field_value) if ($field_value['raw'] && array_key_exists($field_name, $toolset_fields)) {
+            $value = $field_value['raw'];
+            update_post_meta($post->ID, "wpcf-{$field_name}", $value);
+        }
         foreach ($toolset_fields as $field_name => $field ) {
-			if (isset($request[$field_name])) update_post_meta($post->ID, "wpcf-{$field_name}", $request[$field_name]);
-		}
-		return $post;
-	};
-	foreach (get_post_types() as $post_type) add_action( "rest_after_insert_{$post_type}", $after_insert, 10, 3 );
+            if (isset($request[$field_name])) update_post_meta($post->ID, "wpcf-{$field_name}", $request[$field_name]);
+        }
+        return $post;
+    };
+    foreach (get_post_types() as $post_type) add_action( "rest_after_insert_{$post_type}", $after_insert, 10, 3 );
 }, 99);
 function custom_rest_link_translation($data) {
-	if (!isset($data['left'])) return new WP_Error( 'missing_left', 'Missing left parameter', array('status' => 500) );
-	if (!isset($data['right'])) return new WP_Error( 'missing_right', 'Missing right parameter', array('status' => 500) );
-	if (!isset($data['lang'])) return new WP_Error( 'missing_lang', 'Missing lang parameter', array('status' => 500) );
+    if (!isset($data['left'])) return new WP_Error( 'missing_left', 'Missing left parameter', array('status' => 500) );
+    if (!isset($data['right'])) return new WP_Error( 'missing_right', 'Missing right parameter', array('status' => 500) );
+    if (!isset($data['lang'])) return new WP_Error( 'missing_lang', 'Missing lang parameter', array('status' => 500) );
 
-	$left = get_post((int) $data['left']);
-	if (!$left) return new WP_Error( 'missing_left', 'Invalid left post id', array('status' => 500) );
-	$right = get_post((int) $data['right']);
-	if (!$right) return new WP_Error( 'missing_right', 'Invalid right post id', array('status' => 500) );
-	$lang = $data['lang'];
+    $left = get_post((int) $data['left']);
+    if (!$left) return new WP_Error( 'missing_left', 'Invalid left post id', array('status' => 500) );
+    $right = get_post((int) $data['right']);
+    if (!$right) return new WP_Error( 'missing_right', 'Invalid right post id', array('status' => 500) );
+    $lang = $data['lang'];
 
-	$wpml_element_type = apply_filters( 'wpml_element_type', $left->post_type );
+    $wpml_element_type = apply_filters( 'wpml_element_type', $left->post_type );
     $get_language_args = array('element_id' => $left->ID, 'element_type' => $left->post_type );
     $original_post_language_info = apply_filters( 'wpml_element_language_details', null, $get_language_args );
     $set_language_args = [
